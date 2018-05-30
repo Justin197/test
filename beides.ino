d8fa3931cea1cd2851fe5c9d9293555f0d44489e
Justin197 <39736672+Justin197@users.noreply.github.com>
2018-05-30 12:59:03 +0200

    Add files via upload
    Arduino program

---
+++ beides.ino
@@ -0,0 +1,47 @@
+int flash = 6;
+int analogPin = 0;
+int ldrWert = 0;
+
+int x = 0;
+int led = 13;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(flash, OUTPUT);
+  pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+
+
+  //char ard_sends = '1';
+  //Serial.print (ldrWert);
+  digitalWrite(flash, HIGH);
+  ldrWert = analogRead(analogPin);
+  if (ldrWert < 900) {
+    Serial.println(9);
+    delay(2000);
+  }
+  if (ldrWert > 900) {
+    Serial.println(10); //leer
+    delay(2000);
+  }
+  if (Serial.available() > 0) {
+
+
+
+    x = Serial.read();
+
+    if (x == '1') {
+      digitalWrite(led, HIGH);
+    }
+    else if (x == '0') {
+      digitalWrite(led, LOW);
+    }
+    else {
+      Serial.println("invalid!");
+
+    }
+  }
+}
