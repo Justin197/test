6431af1ac9139b0fdddad4a4065579f9866552ba
ledi111 <39736619+ledi111@users.noreply.github.com>
2018-05-31 13:47:26 +0200

    Erweiterung
    Briefkasten schließt automatisch

--- beides.ino
+++ beides.ino
@@ -1,47 +1,114 @@
 int flash = 6;
+
 int analogPin = 0;
+
 int ldrWert = 0;
 
+int schalter = 7;
+
+int buttonState= LOW;
+
 int x = 0;
+
 int led = 13;
 
+
+
 void setup() {
+
   Serial.begin(9600);
+
   pinMode(flash, OUTPUT);
+
   pinMode(led, OUTPUT);
 
+  pinMode(schalter, INPUT);
+
+
+
 }
+
+
 
 void loop() {
 
 
+
+
+
   //char ard_sends = '1';
+
   //Serial.print (ldrWert);
+
   digitalWrite(flash, HIGH);
+
   ldrWert = analogRead(analogPin);
+
   if (ldrWert < 900) {
+
     Serial.println(9);
+
     delay(2000);
+
   }
+
   if (ldrWert > 900) {
+
     Serial.println(10); //leer
+
     delay(2000);
+
   }
+
   if (Serial.available() > 0) {
+
+
+
+
 
 
 
     x = Serial.read();
 
+
+
     if (x == '1') {
+
       digitalWrite(led, HIGH);
-    }
-    else if (x == '0') {
+      //delay(5000);
+
+      buttonState = digitalRead(schalter);
+
+      while (buttonState==LOW){
+      buttonState = digitalRead(schalter);
+      delay(1000);
+   }
+      
+    
+      
+      
       digitalWrite(led, LOW);
+
+      Serial.println(6);
+      delay(1000);
+      Serial.println(6);
+      
     }
-    else {
-      Serial.println("invalid!");
+
+    else if (x == '0') {
+
+      digitalWrite(led, LOW);
 
     }
+
+    else {
+
+      Serial.println("invalid!");
+
+
+
+    }
+
   }
+
 }
