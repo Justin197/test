83c91bb6230e0a5609ee12c167bc5f7e29860e61
ledi111 <39736619+ledi111@users.noreply.github.com>
2018-05-31 13:45:59 +0200

    Erweiterung
    Durch schließen des Kastens lässt er sich wieder automatisch abschließen.

--- beides.py
+++ beides.py
@@ -23,30 +23,45 @@ while True:
 
     if(z==9):
         print("jipi")
-    if (z==10):
-        continue
-    while y not in ['x']:
         y = input()
+
+
 
         if y == '0':
             print("LED OFF")
             ser.write(bytes('0', 'UTF-8'))
-            
+            break
         if y == '1':
             print("LED ON")
             ser.write(bytes('1', 'UTF-8'))
-           
+
+            time.sleep(2)
+            
+            ser_bytes = ser.readline()
+            ser.flushInput()
+            print(ser_bytes)
+            x = ser.readline()
+            y=x.decode('utf-8')
+            
+            print(y)
+            u=int (y)
+            print(u)
+            #time.sleep(2)
+            while (u!=6):
+                print("super")
+                time.sleep(2)
+            print("xyz")
+            
+            
+            
+    if (z==10):
+        continue
+    
     time.sleep(2)
     continue
-    #if(z==2):
-    #    print("noho")
-    #break
-
-#y = ''
-#print(y)
+    
 
 
 
 
 time.sleep(2)
-
